ef563d95f85d38322fc0925fcafd743cf64af58b
Yuri Cardozo <103226844+Netunosz@users.noreply.github.com>
2024-02-13 01:31:41 -0300

    Update Estrelas.js

---
+++ Estrelas.js
@@ -0,0 +1,20 @@
+function pulaLinha() {
+
+    document.write("<br>");
+}
+
+function mostra(frase) {
+
+    document.write(frase);
+    pulaLinha();
+}
+
+for(var linha = 1; linha <= 3; linha++) {
+
+    for(var coluna = 1; coluna <= 10; coluna++) {
+
+        document.write("*");		
+    }
+
+    pulaLinha();
+}
